65c1285fa6b1320492063cd15581210899f27e99
John Goytia <JGoytia@ucvts.tec.nj.us>
2019-11-04 13:18:39 -0500

    commit 4

--- src/parity.js
+++ src/parity.js
@@ -1,2 +1,19 @@
 const readlineSync = require("readline-sync");
 const amount = Number(readlineSync.question("\nEnter an integer: "));
+
+if (amount < Number.MIN_SAFE_INTEGER){
+  console.log("\nInvalid.")
+
+}else if (!Number.isInteger(amount)){
+  console.log("\nInvalid.")
+
+} else if (amount > Number.MAX_SAFE_INTEGER){
+  console.log("\nInvalid.")
+
+}else if (amount == 0){
+  console.log("\nEven.")
+}else if (amount%2 == 0){
+  console.log("Even.")
+}else {
+  console.log("Odd.")
+}
