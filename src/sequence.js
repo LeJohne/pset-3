ecccec1bbede6905c3b63077d323c31a6b50f325
LeJohne <johngoy11024@gmail.com>
2019-11-10 17:24:46 -0500

    finished?

--- src/sequence.js
+++ src/sequence.js
@@ -1,27 +1,27 @@
 const readlineSync = require("readline-sync");
-
-console.log("\nEnter three numbers.\n")
-const amount1 = Number(readlineSync.question(""));
+const amount1 = Number(readlineSync.question("\nEnter three numbers.\n\n"));
 const amount2 = Number(readlineSync.question(""));
 const amount3 = Number(readlineSync.question(""));
 
 let diff1 = amount1 - amount2;
 let diff2 = amount2 - amount3;
 
-if (amount1 < Number.MIN_SAFE_INTEGER || amount1 > Number.MAX_SAFE_INTEGER){
-  console.log("\nInvalid")
-} else if (amount2 < Number.MIN_SAFE_INTEGER || amount2 > Number.MAX_SAFE_INTEGER){
-  console.log("\nInvalid")
-} else if (amount3 < Number.MIN_SAFE_INTEGER || amount3 > Number.MAX_SAFE_INTEGER){
-  console.log("\nInvalid")
-} else if(amount3 > amount2 && diff2 == diff1){
-  console.log("Stictly increasing.")
-} else if(amount3 > amount2 && amount2 > amount1 == diff1){
-  console.log("Increasing.")
-} else if(amount3 == amount2 && amount2 == amount1 == diff1){
-  console.log("Equal.")
-} else if(amount3 < amount2 && amount2 < amount1 == diff1){
-  console.log("Decreasing.")
-} else if(amount3 < amount2 && diff1 == diff2){
-  console.log("Strictly Decreasing.")
+if (amount1 < Number.MIN_SAFE_INTEGER || amount1 >  Number.MAX_SAFE_INTEGER) {
+    console.log("\nInvalid.")
+}else if (amount2 < Number.MIN_SAFE_INTEGER || amount2 >  Number.MAX_SAFE_INTEGER) {
+    console.log("\nInvalid.")
+}else if (amount3 < Number.MIN_SAFE_INTEGER || amount3 >  Number.MAX_SAFE_INTEGER) {
+    console.log("\nInvalid.")
+}else if (amount3 > amount2 && diff2 == diff1){
+    console.log("\nIncreasing.")
+}else if (amount3 > amount2 && amount2 > amount1){
+    console.log("\nStrictly Increasing.")
+}else if (amount3 < amount2 && diff2 == diff1){
+    console.log("\nDecreasing.")
+}else if (amount3 < amount2 && amount2 < amount1) {
+   console.log("\nStrictly Decreasing.")
+}else if (amount3 == amount2 && amount2 == amount1){
+  console.log("\nEqual.")
+}else {
+  console.log("\nUnordered")
 }
---
+++ src/temperature.js
@@ -0,0 +1,34 @@
+const readlineSync = require("readline-sync");
+const temp = Number(readlineSync.question("\nEnter a temperature: "));
+const scale = String(readlineSync.question("Enter a scale: "));
+
+const fkelvin = 273.15
+const bkelvin = 373.15
+const fcelsius = 0
+const bcelsius = 100
+const ffar = 32
+const bfar = 212
+
+if (temp < (Number.MIN_SAFE_INTEGER) || temp > Number.MAX_SAFE_INTEGER) {
+      console.log("\nInvalid.")
+} else if (temp <= ffar && (scale == "F" || scale == "f")){
+      console.log("\nSolid.")
+} else if ((temp > ffar && temp < bfar) && (scale == "F" || scale == "f")){
+      console.log("\nLiquid.")
+} else if (temp >= bfar && (scale == "F" || scale == "f")){
+      console.log("\nGas.")
+} else if (temp <= fcelsius && (scale == "C" || scale == "c")){
+      console.log("\nSolid.")
+} else if ((temp > fcelsius && temp < bcelsius) && (scale == "C" || scale == "c")){
+      console.log("\nLiquid.")
+} else if (temp >= bcelsius && (scale == "C" || scale == "c")){
+      console.log("\nGas.")
+} else if (temp <= fkelvin && (scale == "K" || scale == "k")){
+      console.log("\nSolid.")
+} else if ((temp > fkelvim && temp < bkelvin) && (scale == "K" || scale == "k")){
+      console.log("\nLiquid.")
+} else if (temp >= bkelvin && (scale == "K" || scale == "k")){
+      console.log("\nGas.")
+} else {
+      console.log("\nInvalid.")
+}
